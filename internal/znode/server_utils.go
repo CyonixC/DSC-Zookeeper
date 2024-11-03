45579cf7e4614b5a24edb0e047bf35c8b9c0e259
Lee Darren <hecatehonryuu@gmail.com>
2024-11-03 18:51:06 +0800

    Implemented server_utils, untested
    Yet to implement watch and seq flag functionality, will need to figure those out

--- internal/znode/server_utils.go
+++ internal/znode/server_utils.go
@@ -6,45 +6,135 @@ package znode
 //TODO stuff to sync with other servers
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 )
 
 // InitZnodeServer ensures the znode directory exists and returns an empty znode cache for znode instances.
-func InitZnodeServer() map[string]*ZNode {
+func InitZnodeServer() (map[string]*ZNode, error) {
 	//create a directory for znodes, mkdirall will not error if it already exists
 	err := os.MkdirAll(filepath.Join(".", znodeDir), 0755)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	// Cache is meant to reduce unnecessary interactions with file system.
 	// znode might be in storage but not in cache. Vice versa should not happen.
 	// Might be safer to not use cache and always read from storage, not sure.
 	znodeCache := make(map[string]*ZNode)
-	return znodeCache
+	return znodeCache, nil
 }
 
-func Create(path string, data []byte, ephemeral bool, sequential bool) {
+// Create creates a znode in the filesystem and adds to cache.
+// Returns the name of the znode created, or an error if znode already exists
+// If sequential flag is set, znode name will be modified to include a sequence number
+func Create(znodeCache map[string]*ZNode, path string, data []byte, ephemeral bool, sequential bool) (string, error) {
+
+	if sequential {
+		//TODO modify path according to sequential flag behaviour, probably making us of childrenZnode(path)
+	}
+
+	//version numbering starting at 1
+	_, err := createZnode(znodeCache, path, 1, ephemeral, data)
+
+	if err != nil {
+		return "", err
+	}
+
+	//get last part of path, i.e. the znode name
+	name := filepath.Base(path)
+	return name, nil
 
 }
 
-func Delete(path string, version int) {
+// Delete deletes a znode in the filesystem and removes from cache.
+// currently if version matches, is a recursive delete, do we newed to check for children?
+func Delete(znodeCache map[string]*ZNode, path string, version int) error {
+	znode, err := getZnode(znodeCache, path)
+	if err != nil {
+		return err
+	}
+
+	if znode.Version != version {
+		return errors.New("version does not match")
+	}
+
+	err = deleteZnode(znode)
+	if err != nil {
+		return err
+	}
+	//TODO watch flag behaviour
+	//remove from cache
+	znodeCache[path] = nil
+
+	return nil
+}
+
+// Exists checks if a znode corresponding to the provided path exists.
+func Exists(znodeCache map[string]*ZNode, path string, watch bool) bool {
+	exists := existsZnode(znodeCache, path)
+
+	if exists && watch {
+		//TODO watch flag behaviour
+	}
+
+	return exists
 
 }
 
-func Exists(path string, watch bool) {
+// GetData retrieves the data of a znode from the filesystem.
+// Also returns the znode struct for relavant metadata.
+func GetData(znodeCache map[string]*ZNode, path string, watch bool) ([]byte, *ZNode, error) {
+	znode, err := getZnode(znodeCache, path)
+	if err != nil {
+		return nil, nil, err
+	}
+	data, err := readZnode(znode)
+	if watch {
+		//TODO watch flag behaviour
+	}
+	//assumes relavant metadata stored in znode struct, i.e. version
+	return data, znode, err
 
 }
 
-func GetData(path string, watch bool) {
+func SetData(znodeCache map[string]*ZNode, path string, data []byte, version int) error {
+	znode, err := getZnode(znodeCache, path)
+	if err != nil {
+		return err
+	}
 
+	if znode.Version != version {
+		return errors.New("version does not match")
+	}
+
+	err = writeZNode(znode, data)
+	if err != nil {
+		return err
+	}
+
+	//TODO watch flag behaviour
+
+	return nil
 }
 
-func SetData(path string, data []byte, version int) {
+// GetChildren retrieves the children of a znode from the filesystem.
+func GetChildren(znodeCache map[string]*ZNode, path string, watch bool) ([]string, error) {
+	exists := existsZnode(znodeCache, path)
+	if !exists {
+		return nil, errors.New("znode does not exist")
+	}
 
-}
+	children, err := childrenZnode(path)
+	if err != nil {
+		return nil, err
+	}
 
-func GetChildren(path string, watch bool) {
+	if watch {
+		//TODO watch flag behaviour
+	}
+
+	return children, nil
 
 }
 
--- internal/znode/znode.go
+++ internal/znode/znode.go
@@ -3,6 +3,7 @@ package znode
 //functions in this file are not meant to be exported, only used by other znode package files
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -18,69 +19,72 @@ import (
 const znodeDir = "znodeDir"
 
 type ZNode struct {
-	//path provided by client will be a relative path in znodeDir, corresponding to znodepath above
-	path string //TODO func to modify filepath to match local sys and include seq number, to be handled by server_utils prob using childrenZnode
+	//Path provided by client will be a relative Path in znodeDir, corresponding to znodepath above
+	Path string //TODO func to modify filepath to match local sys and include seq number, to be handled by server_utils prob using childrenZnode
 	//Not sure if path should be stored in znode, might be unnecessary with cache, but leaving for now
 	// data       []byte //tentatively don't want to store data here, should not need it since only related ops are read and write
-	version    int
-	ephermeral bool
+	Version    int
+	Ephermeral bool
+	//TODO implement watch, this will have to be session specific? Not sure yet
 }
 
-// createZnode creates a znode in the filesystem and returns the znode.
-// will panic if znode already exists
-func createZnode(znodeCache map[string]*ZNode, path string, version int, ephermeral bool, data []byte) *ZNode {
+// createZnode creates a znode in the filesystem and adds to cache.
+// will error if znode already exists, check and handle
+func createZnode(znodeCache map[string]*ZNode, path string, version int, ephermeral bool, data []byte) (*ZNode, error) {
 	if existsZnode(znodeCache, path) {
-		panic("Znode already exists")
+		return nil, errors.New("znode already exists")
 	}
 
 	znode := &ZNode{
-		path:       path,
-		version:    version, //May not start at 1, i.e. syncing with other servers
-		ephermeral: ephermeral,
+		Path:       path,
+		Version:    version, //May not start at 1, i.e. syncing with other servers
+		Ephermeral: ephermeral,
 	}
-	znodeCache[path] = znode
-	writeZNode(znode, data)
+	err := writeZNode(znode, data)
+	if err != nil {
+		return nil, err
+	}
 
-	return znode
+	znodeCache[path] = znode
+
+	return znode, nil
 }
 
-// writeZNode creates a znode in the filesystem.
-// does not check if all dir(znode parents) in path exists, will error if they do not
-func writeZNode(znode *ZNode, data []byte) {
-	path := filepath.Join(".", znodeDir, znode.path, strconv.Itoa(znode.version)+".txt")
+// writeZNode creates/overwrites znode in the filesystem, returns nil if successful.
+// Does not handle version checking and will overwrite if version exists, be sure to check version
+func writeZNode(znode *ZNode, data []byte) error {
+	path := filepath.Join(".", znodeDir, znode.Path, strconv.Itoa(znode.Version)+".txt")
+	//TODO modify implementation such that will error if overwriting existing version (if we establish that should not happen)
 
 	//create a file in the path
-	err2 := os.WriteFile(path, data, 0644)
-	if err2 != nil {
-		panic(err2)
-	}
+	err := os.WriteFile(path, data, 0644)
+	return err
 }
 
-// readZnode reads a znode from the filesystem.
-// Updates the data field of the znode.
+// readZnode reads a znode of from the filesystem.
 // Does not handle version checking.
-func readZnode(znode *ZNode) []byte {
-	path := filepath.Join(".", znodeDir, znode.path, strconv.Itoa(znode.version)+".txt")
+func readZnode(znode *ZNode) ([]byte, error) {
+	path := filepath.Join(".", znodeDir, znode.Path, strconv.Itoa(znode.Version)+".txt")
 	//read the file in the path
 	data, err := os.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-
-	return data
+	return data, err
 }
 
-// deleteZnode deletes a znode (version) from the filesystem.
-// Assumes the znode exists.
+// deleteZnode deletes a znode (version) from the filesystem, returns nil if successful.
 // Does not handle version checking.
-func deleteZnode(znode *ZNode) bool {
-	path := filepath.Join(".", znodeDir, znode.path, strconv.Itoa(znode.version)+".txt")
+func deleteZnodeVer(znode *ZNode) error {
+	path := filepath.Join(".", znodeDir, znode.Path, strconv.Itoa(znode.Version)+".txt")
 	err := os.Remove(path)
-	if err != nil {
-		panic(err)
-	}
-	//tentatively returns true but prob unnecessary
-	return true
+	return err
+}
+
+// deleteZnode deletes a znode from the filesystem, returns nil if successful.
+// Does not handle version checking nor check for children znodes.
+// Remember to remove from cache after calling this function.
+func deleteZnode(znode *ZNode) error {
+	err := os.RemoveAll(filepath.Join(".", znodeDir, znode.Path))
+	return err
+
 }
 
 // existsZnode checks if a znode with specified path exists.
@@ -99,12 +103,12 @@ func existsZnode(znodeCache map[string]*ZNode, path string) bool {
 }
 
 // latestVersion returns the latest stored version of a znode.
-// Not sure if need, might be useful
-func latestVersion(path string) int {
+// Returns -1 if error is encountered.
+func latestVersion(path string) (int, error) {
 	//get all files in the path
 	entries, err := os.ReadDir(filepath.Join(".", znodeDir, path))
 	if err != nil {
-		panic(err)
+		return -1, err
 	}
 
 	//find the latest version
@@ -117,45 +121,48 @@ func latestVersion(path string) int {
 
 		//parse the filename to get the version number
 		version, err := strconv.Atoi(entry.Name()[:len(entry.Name())-4])
-		//should error if the filename is not a number
+		//should error if encounter a filename that is not a number
 		if err != nil {
-			panic(err)
+			return -1, err
 		}
 
 		if version > latest {
 			latest = version
 		}
 	}
-	return latest
+	return latest, nil
 }
 
 // getZnode returns a znode with the specified path.
-// If not in cache will check if in storage
-// not sure if need, meant for cases where znode is not in cache but is in storage
-func getZnode(znodeCache map[string]*ZNode, path string) *ZNode {
+// If not in cache will check if in storage and add to cache.
+func getZnode(znodeCache map[string]*ZNode, path string) (*ZNode, error) {
 	if znode, exists := znodeCache[path]; exists {
-		return znode
+		return znode, nil
 	}
 	if existsZnode(znodeCache, path) {
+		version, err := latestVersion(path)
+		if err != nil {
+			return nil, err
+		}
 		znode := &ZNode{
-			path:       path,
-			version:    latestVersion(path),
-			ephermeral: false, //This might be an issue, tentatively assuming if znode instance is not in cache but in storage, it is not ephermeral
+			Path:       path,
+			Version:    version,
+			Ephermeral: false, //This might be an issue, tentatively assuming if znode instance is not in cache but in storage, it is not ephermeral
 		}
 		znodeCache[path] = znode
-		return znode
+		return znode, nil
 	}
 	//return nil if znode does not exist
-	return nil
+	return nil, errors.New("znode does not exist")
 }
 
 // children returns the children of a znode.
 // Will be empty if znode has no children.
-func childrenZnode(path string) []string {
+func childrenZnode(path string) ([]string, error) {
 	//get all files in the path
 	entries, err := os.ReadDir(filepath.Join(".", znodeDir, path))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	children := make([]string, 0)
@@ -165,5 +172,5 @@ func childrenZnode(path string) []string {
 			children = append(children, entry.Name())
 		}
 	}
-	return children
+	return children, nil
 }
