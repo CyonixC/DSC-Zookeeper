9287fd0613209430162d74648a1de7e973dc2547
DeProfessorA <aureliusyuwana@gmail.com>
2024-11-14 11:40:16 +0800

    feat: Docker attempt

---
+++ dockerfile
@@ -0,0 +1,26 @@
+# syntax=docker/dockerfile:1.7-labs
+
+FROM golang:1.23
+
+# Set destination for COPY
+WORKDIR /app
+
+# Download Go modules
+COPY go.mod ./
+RUN go mod download
+
+COPY --exclude=go.mod --exclude=config.json . ./
+# Build
+RUN CGO_ENABLED=0 GOOS=linux go build -o ./go-main ./dockertest/main.go
+
+# To bind to a TCP port, runtime parameters must be supplied to the docker command.
+# But we can (optionally) document in the Dockerfile what ports
+# the application is going to listen on by default.
+# https://docs.docker.com/engine/reference/builder/#expose
+EXPOSE 8080
+
+# Download IP configs
+COPY config.json ./
+
+# Run
+CMD [ "./go-main" ]
---
+++ dockertest/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	connectionManager "local/zookeeper/internal/ConnectionManager"
+	"local/zookeeper/internal/logger"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Servers []string `json:"servers"`
+	Clients []string `json:"clients"`
+}
+
+func main() {
+	mode := os.Getenv("MODE") // "Server" or "Client"
+
+	if mode == "Server" {
+		server_name := os.Getenv("NAME")
+		startServer(server_name)
+	} else {
+		config := loadConfig("config.json")
+		client_name := os.Getenv("NAME")
+		startClient(client_name, config)
+	}
+}
+
+func startServer(server_name string) {
+	logger.Info(fmt.Sprint("Starting server ", server_name))
+	recv := connectionManager.Init()
+	for {
+		msg := <-recv
+		logger.Info(fmt.Sprint("Recv ", string(msg.Message), " from ", msg.Remote))
+		time.Sleep(time.Second)
+	}
+}
+
+func startClient(client_name string, config Config) {
+	logger.Info(fmt.Sprint("Starting server ", client_name))
+	for {
+		for _, server := range config.Servers {
+			data := []byte("hello world" + client_name)
+			remoteAddr := server + ":8080"
+			connectionManager.SendMessage(connectionManager.NetworkMessage{remoteAddr, data})
+			logger.Info(fmt.Sprint("Sent hello to ", remoteAddr))
+		}
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func loadConfig(filename string) Config {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Failed to open config file: %v", err)
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Failed to read config file: %v", err)
+	}
+
+	var config Config
+	if err := json.Unmarshal(bytes, &config); err != nil {
+		log.Fatalf("Failed to parse config file: %v", err)
+	}
+
+	return config
+}
