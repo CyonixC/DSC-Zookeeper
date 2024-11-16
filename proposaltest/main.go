20660bf5d96c3a05bdea6c9211f754db6a738e93
Jav <35833065+CyonixC@users.noreply.github.com>
2024-11-16 20:15:40 +0800

    test: demo for proposal system

--- proposaltest/main.go
+++ proposaltest/main.go
@@ -5,7 +5,6 @@ import (
 	cxn "local/zookeeper/internal/ConnectionManager"
 	prp "local/zookeeper/internal/Proposals"
 	"local/zookeeper/internal/logger"
-	"log"
 	"log/slog"
 	"os"
 	"time"
@@ -17,19 +16,25 @@ func receiveMsg(recv_channel chan cxn.NetworkMessage) {
 	}
 }
 
-func identity(m []byte) ([]byte, error) { return m, nil }
+func identity(m []byte) ([]byte, error)  { return m, nil }
+func rejectAll(m []byte) ([]byte, error) { return m, fmt.Errorf("denied") }
 
 func main() {
 	role := os.Getenv("MODE")
-	handler := logger.NewPlainTextHandler(slog.LevelInfo)
+	handler := logger.NewPlainTextHandler(slog.LevelDebug)
 	lg := slog.New(handler)
 	logger.InitLogger(lg)
 	recv_channel, failed := cxn.Init()
-	commitChan := prp.Init(identity)
+	commitChan, denied := prp.Init(rejectAll)
 	go receiveMsg(recv_channel)
 	go func() {
 		for str := range failed {
-			log.Println("Failed to send to", str)
+			logger.Info("Failed to send to", str)
+		}
+	}()
+	go func() {
+		for deniedReq := range denied {
+			logger.Warn(fmt.Sprint("Request #", deniedReq.ReqNumber, " denied"))
 		}
 	}()
 
@@ -41,8 +46,9 @@ func main() {
 	if role == "Client" {
 		var input string
 		for {
-			fmt.Scan(&input)
-			prp.SendWriteRequest([]byte(input))
+			fmt.Scanln(&input)
+			logger.Info(fmt.Sprint("Sending message: ", input))
+			prp.SendWriteRequest([]byte(input), 0)
 		}
 	}
 	time.Sleep(time.Hour)
