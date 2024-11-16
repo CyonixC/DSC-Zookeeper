ddcfc5758d9a71ac3e2621b444473901facfb27b
Jav <35833065+CyonixC@users.noreply.github.com>
2024-11-16 15:21:10 +0800

    feat: working demo for proposals (not committing the modified Dockerfile)

--- internal/ConnectionManager/config.go
+++ internal/ConnectionManager/config.go
@@ -5,4 +5,5 @@ const tcpRetryConnectionTimeoutSeconds int = 2
 const tcpWriteTimeoutSeconds int = 2
 const tcpEstablishTimeoutSeconds int = 30
 
-var ip_list = []string{"server1", "server2"}
+// TODO make this reference the Docker config
+var ip_list = []string{"server1", "server2", "server3"}
