af4c0b9b72aaa094df51e53dba992f08fafcffd5
Jav <35833065+CyonixC@users.noreply.github.com>
2024-11-27 00:40:55 +0800

    test: make stress test more stressful

--- cxnStressTest/cxnStressTest.go
+++ cxnStressTest/cxnStressTest.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
+	configReader "local/zookeeper/internal/ConfigReader"
 	connectionManager "local/zookeeper/internal/ConnectionManager"
 	"local/zookeeper/internal/logger"
 	"log/slog"
+	"strconv"
 	"time"
 )
 
 // Stress test for Docker + ConnectionManager
+var prev []string
 
 func main() {
-	handler := logger.NewColouredTextHandler(slog.LevelDebug)
+	handler := logger.NewColouredTextHandler(slog.LevelInfo)
 	logger.InitLogger(slog.New(handler))
+	prev = make([]string, len(configReader.GetConfig().Servers))
 
 	recv, failed := connectionManager.Init()
+	time.Sleep(time.Second)
 	go func() {
 		for f := range failed {
 			logger.Fatal(fmt.Sprint("Failed to send to ", f))
@@ -22,12 +27,15 @@ func main() {
 	}()
 	go func() {
 		for r := range recv {
-			logger.Info(fmt.Sprint("Received: ", string(r.Message)))
+			time.Sleep(time.Millisecond)
+			remoteId, _ := strconv.Atoi(string(r.Remote[len(r.Remote)-1]))
+			logger.Info(fmt.Sprint("Received: ", string(r.Message), " from ", string(r.Remote), "; previous: ", prev[remoteId-1]))
+			prev[remoteId-1] = string(r.Message)
 		}
 	}()
 
 	for i := 0; ; i++ {
-		time.Sleep(time.Second)
+		time.Sleep(time.Microsecond * 200)
 		connectionManager.Broadcast([]byte(fmt.Sprint("Testing ", i)))
 	}
 }
