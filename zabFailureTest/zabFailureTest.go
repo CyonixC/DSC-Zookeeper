336cfa170bf07c43ec794a409ca0d11f1c3df99d
Jav <35833065+CyonixC@users.noreply.github.com>
2024-12-05 18:24:59 +0800

    test: can elect new leader, new leader unresponsive

--- zabFailureTest/zabFailureTest.go
+++ zabFailureTest/zabFailureTest.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	configReader "local/zookeeper/internal/ConfigReader"
 	connectionManager "local/zookeeper/internal/ConnectionManager"
 	proposals "local/zookeeper/internal/Proposals"
+	"local/zookeeper/internal/election"
 	"local/zookeeper/internal/logger"
 	"log/slog"
 	"math/rand/v2"
@@ -14,7 +17,6 @@ import (
 // Fault tolerance tests for ZAB servers
 func randomPassFail(proposed []byte) ([]byte, error) {
 	if rand.IntN(2) == 0 {
-		logger.Debug(fmt.Sprint("Current ZXID: ", proposals.ZxidCounter.GetLatestZXID()))
 		return proposed, errors.New("Failed")
 	} else {
 		return proposed, nil
@@ -28,16 +30,48 @@ func main() {
 	recv, failed := connectionManager.Init()
 	time.Sleep(time.Second)
 	go func() { //Removed listener from proposals package, call ProcessZabMessage manually
+		electing := false
 		for network_msg := range recv {
-			proposals.ProcessZabMessage(network_msg)
+			switch network_msg.Type {
+			case connectionManager.ZAB:
+				if electing {
+					// If election currently happening, enqueue all ZAB messages
+					proposals.StoreZabMessage(network_msg)
+				} else {
+					proposals.ProcessZabMessage(network_msg)
+				}
+			case connectionManager.ELECTION:
+				var messageWrapper election.MessageWrapper
+				err := json.Unmarshal(network_msg.Message, &messageWrapper)
+				if err != nil {
+					logger.Fatal(fmt.Sprint("Error unmarshalling message:", err))
+				}
+				currentlyElecting := election.HandleMessage(messageWrapper)
+				if electing && !currentlyElecting {
+					// If the election has just finished, process all Zab messages received during the election.
+					proposals.EmptyMessageQueue()
+				}
+				electing = currentlyElecting
+			}
 		}
 	}()
-	success, rejected := proposals.Init(randomPassFail)
+	success, rejected, counter := proposals.Init(randomPassFail)
+	if counter == nil {
+		logger.Fatal("Failed")
+	}
+	election.ElectionInit(counter)
+	time.Sleep(10 * time.Second)
+	if configReader.GetName() == "server1" {
+		election.InitiateElectionDiscovery()
+	}
 	time.Sleep(10 * time.Second)
 
 	go func() {
 		for f := range failed {
-			logger.Fatal(fmt.Sprint("Failed to send to ", f))
+			logger.Error(fmt.Sprint("Failed to send to ", f))
+			if f == election.Coordinator.GetCoordinator() {
+				go election.InitiateElectionDiscovery()
+			}
 		}
 	}()
 
