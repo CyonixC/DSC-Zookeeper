b6f113b87192e9dbbf56531b9350ad65877284c4
Jav <35833065+CyonixC@users.noreply.github.com>
2024-12-06 02:05:44 +0800

    conf: decrease tcp timeout

--- internal/ConnectionManager/config.go
+++ internal/ConnectionManager/config.go
@@ -5,4 +5,4 @@ import "time"
 const portNum int = 8080
 const tcpRetryConnectionTimeoutSeconds int = 1
 const tcpWriteTimeoutSeconds int = 2
-const tcpEstablishTimeout = 500 * time.Millisecond
+const tcpEstablishTimeout = 50 * time.Millisecond
